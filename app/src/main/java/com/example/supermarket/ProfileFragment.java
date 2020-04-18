1100bc7e3f650cba6a37e381ede7658e7e875d0f
haryandrafatwa <haryandrafatwa010@gmail.com>
2020-04-18 20:04:01 +0700

    Fix Profile Page

--- app/src/main/java/com/example/supermarket/ProfileFragment.java
+++ app/src/main/java/com/example/supermarket/ProfileFragment.java
@@ -139,7 +139,6 @@ public class ProfileFragment extends Fragment {
                         }
                     }
                     mBadge.setNumber(counter);
-                    Toast.makeText(getActivity(), counter+"", Toast.LENGTH_SHORT).show();
                 }
             }
 
